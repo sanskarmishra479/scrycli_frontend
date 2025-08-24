4bcb1ff875940fda95ea36f92b9b60d29aa3887b
Sanskar Mishra <sanskarmishra479@gmail.com>
2025-08-24 22:57:51 +0530

    add change in firebase api

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "scrycli-4a92c.firebaseapp.com",
   projectId: "scrycli-4a92c",
   storageBucket: "scrycli-4a92c.firebasestorage.app",
