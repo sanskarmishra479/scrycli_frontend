c0a3703be322e47864a7365d98e7bff838979072
Sanskar Mishra <sanskarmishra479@gmail.com>
2025-08-30 17:26:18 +0530

    add install command

--- src/components/Codesnippte.tsx
+++ src/components/Codesnippte.tsx
@@ -24,7 +24,7 @@ export const CodeSnippte = () => {
             transition={{ duration: 0.5 }}
             className="hidden h-20 w-full md:flex items-center justify-center">
             <div className="bg-white min-h-10 min-w-70 flex items-center justify-between pl-4 pr-2 rounded-lg gap-2 border-1 border-slate-200">
-                <h1 ref={codeRef} className="font-[Poppins] text-sm text-slate-600">npm install -g scrycli</h1>
+                <h1 ref={codeRef} className="font-[Poppins] text-sm text-slate-600">npm install -g @sanskarmishra479/scrycli</h1>
                 <motion.div 
                     className="bg-black text-white h-8 w-8 flex items-center justify-center rounded-lg cursor-pointer"
                     whileHover={{ scale: 1.05 }}
